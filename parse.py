6f70c72dad8205dc86783dbcb4d8311ed6157ce0
Delyan Kratunov <delyan.kratunov@gmail.com>
2012-02-06 23:03:41 +0000

    Implement basic parsing + collating local scripts

--- parse.py
+++ parse.py
@@ -1,15 +1,72 @@
 import lxml.etree as ET
-from StringIO import StringIO
+import os.path
+
+from django.conf import settings
 
 def find_scripts(data):
-    parser = None
-    if not hasattr(data, 'read'):
-        parser = ET.parse(StringIO(data))
+    if hasattr(data, 'read'):
+        data = data.read()
+    
+    root = ET.HTML(data)
+
+    scripts = root.xpath("//script")
+    javascripts = []
+
+    for script in scripts:
+        keys = script.keys()
+        if ( ('type' in keys and script.get('type').lower() == "text/javascript") or 
+             ('language' in keys and script.get('language').lower() == 'javascript') or
+             ('src' in keys and script.get('src').lower().endswith('js')) or
+             ('type' not in keys and 'language' not in keys) ):
+            # the last condition assumes javascript is the default browser
+            # language (which it should be!)
+            javascripts.append(script)
+    
+    return javascripts
+
+def is_local_script(script):
+    if 'src' in script.keys():
+        src = script.get('src')
+        return src.startswith(settings.MEDIA_URL)
     else:
-        parser = ET.parse(data)
+        return True
 
-    root = parser.getroot()
-    print dir(root)
+def filter_local_scripts(scripts):
+    return filter(is_local_script, scripts)
 
-    return
+def find_local_scripts(data):
+    return filter_local_scripts(find_scripts(data))
 
+def collate_scripts(scripts):
+    parts = []
+    local_script_string = 'page-local script'
+    files = []
+
+    for script in scripts:
+        text = script.text
+
+        if 'src' not in script.keys():
+            parts.append((local_script_string, text))
+        else:
+            url = script.get('src')
+            rel_path = url.replace(settings.MEDIA_URL, "") # remove the web prefix
+            path = os.path.join(settings.MEDIA_ROOT, rel_path) # convert to local path
+            
+            try:
+                # read the actual .js file
+                with open(path, 'r') as f:
+                    files.append(rel_path)
+                    parts.append((rel_path, f.read()))
+            except IOError as e:
+                raise IOError("Could not process script '%s': %s" % (rel_path,
+                                                                     e))
+
+    result = u""""""
+    for source, part in parts:
+        result += u"""// Imported %s\n""" % (source,)
+        result += part.strip() + "\n\n"
+
+    return {'collated': result,
+            'files': files,
+            'scripts': scripts
+           }
--- tests.py
+++ tests.py
@@ -1,13 +1,109 @@
 import parse
 
+from django.conf import settings
 from django.test import TestCase
 
 class ParserTests(TestCase):
     def test_find_scripts(self):
         import urllib2
 
-        data = urllib2.urlopen("http://localhost:8000", timeout=10)
+        data = urllib2.urlopen("http://localhost:8000", timeout=10).read()
 
         scripts = parse.find_scripts(data)
 
         self.assertGreater(len(scripts), 0)
+        
+        data = """
+<html>
+<head>
+    <script>
+        $.magic();
+    </script>
+    <script type="text/javascript">
+        $.magic();
+    </script>
+    <script language="JavaScript">
+        $.magic();
+    </script>
+</head>
+</html>
+        """
+        scripts = parse.find_scripts(data)
+        self.assertEquals(len(scripts), 3)
+
+        data = """
+<html>
+<head>
+    <script type="text/coffeescript">
+        $.magic();
+    </script>
+</head>
+</html>
+        """
+        scripts = parse.find_scripts(data)
+
+        self.assertEquals(len(scripts), 0)
+
+    def test_local_scripts(self):
+        
+        data = """
+<html>
+<head>
+    <script>
+        $.magic();
+    </script>
+    <script type="text/javascript">
+        $.magic();
+    </script>
+    <script language="JavaScript">
+        $.magic();
+    </script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script src="%sjquery.min.js"></script>
+</head>
+</html>
+        """ % settings.MEDIA_URL
+        scripts = parse.find_local_scripts(data)
+        self.assertEquals(len(scripts), 4)
+
+    def test_collate_scripts(self):
+
+        data = """
+<html>
+<head>
+    <script>
+        part1();
+    </script>
+    <script type="text/javascript">
+        part2();
+    </script>
+    <script language="JavaScript">
+        part3();
+    </script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js">
+        must_not_be_included();
+    </script>
+    <script src="%sjs/custom.js">
+        must_not_be_included();
+    </script>
+</head>
+</html> """ % settings.MEDIA_URL
+        
+        result = parse.collate_scripts(parse.find_local_scripts(data))
+        self.assertTrue('js/custom.js' in result['files'])
+        self.assertTrue('must_not_be_included' not in result['collated'])
+        self.assertTrue('part1' in result['collated'])
+        self.assertTrue('part2' in result['collated'])
+        self.assertTrue('part3' in result['collated'])
+        
+
+        data = """
+<html>
+<head>
+    <script src="%sjs/non-existent-script.js">
+        part1();
+    </script>
+</head>
+</html> """ % settings.MEDIA_URL
+        self.assertRaises(IOError, 
+                          lambda: parse.collate_scripts(parse.find_local_scripts(data)))
