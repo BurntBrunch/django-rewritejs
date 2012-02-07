132b16f0180a76204c6999598812a2c41dab49a5
Delyan Kratunov <delyan.kratunov@gmail.com>
2012-02-07 20:28:51 +0000

    Less invasive defaults

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from django.conf import settings
 
 defaults = {'REWRITE_JS_REDIRECT_TAGS_TO_CACHE': False,
             'REWRITE_JS_MIDDLEWARE_INLINE': True,
-            'REWRITE_JS_COLLATE_TAGS_TO_LAST': True,
+            'REWRITE_JS_COLLATE_TAGS_TO_LAST': False,
             'REWRITE_JS_CACHE_PREFIX': "rewritejs/scripts/"}
 
 for key, val in defaults.items():
