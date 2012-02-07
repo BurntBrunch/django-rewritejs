a206c874b101f41709d293b86ce18eaae5d452bd
Delyan Kratunov <delyan.kratunov@gmail.com>
2012-02-07 20:16:39 +0000

    Allow the middleware to work by just adding it to middlewares.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 
 defaults = {'REWRITE_JS_REDIRECT_TAGS_TO_CACHE': False,
-            'REWRITE_JS_MIDDLEWARE_INLINE': False,
+            'REWRITE_JS_MIDDLEWARE_INLINE': True,
             'REWRITE_JS_COLLATE_TAGS_TO_LAST': True,
             'REWRITE_JS_CACHE_PREFIX': "rewritejs/scripts/"}
 
